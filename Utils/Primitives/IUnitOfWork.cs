095a5e63aaa18379612396e95388262ffacbffd2
backsapce <backsapce@hotmail.com>
2024-04-09 12:44:54 +0300

    init

---
+++ Utils/Primitives/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Primitives
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default);
+
+        Task<IDbContextTransaction> BeginTransactionAsync();
+
+        Task CommitTransactionAsync(IDbContextTransaction transaction);
+    }
+}
